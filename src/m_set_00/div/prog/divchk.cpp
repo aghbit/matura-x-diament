604b1bbb0b6fa8702055a7613bd0133ece23673c
Rusiek <szymon.rusiecki@proton.me>
2024-03-25 03:08:19 +0100

    fix interactive program

--- src/m_set_00/div/prog/divchk.cpp
+++ src/m_set_00/div/prog/divchk.cpp
@@ -1,36 +1,43 @@
-#include <cstring>
 #include <fstream>
 #include <iostream>
 
-#include "oi.h"
+void endf(const char* msg, int line, int position) {
+    std::cout << "WRONG" << std::endl;
+    std::cout << "Line " << line << ": " << msg << std::endl;
+    exit(0);
+}
 
 int main(int argc, char ** argv) {
     int n;
     std::ifstream input(argv[1]);
     input >> n;
 
-    bool is_prime;
-    std::ifstream wzo(argv[2]);
-    wzo >> is_prime;
-
     int odp;
-    std::ifstream output(argv[3]);
+    std::ifstream output(argv[2]);
     output >> odp;
 
-    std::cerr << "n = " << n << ", is_prime = " << is_prime << ", odp = " << odp << std::endl;
-    if (is_prime && odp != -1) {
-        std::cerr << "Zły wynik" << std::endl;
-        return 1;
+    if (odp == -1) {
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                std::cout << "WRONG" << std::endl;
+                std::cout << "Dzielnik istnieje" << std::endl;
+                return 0;
+            }
+        }
+        std::cout << "OK" << std::endl;
+        return 0;
     }
     if (odp <= 1 || odp >= n) {
-        std::cerr << "Zwrócona liczba nie może być dzielnikiem zadanej liczby" << std::endl;
-        return 1;
+        std::cout << "WRONG" << std::endl;
+        std::cout << "Zwrócona liczba nie może być dzielnikiem" << std::endl;
+        return 0;
     }
     if (n % odp != 0) {
-        std::cerr << "Zwrócona liczba nie jest dzielnikiem zadanej liczby" << std::endl;
-        return 1;
+        std::cout << "WRONG" << std::endl;
+        std::cout << "Zwrócna liczba nie jest dzielnikiem" << std::endl;
+        return 0;
     }
 
-    std::cout << "Zwrócona liczba jest poprawna" << std::endl;
+    std::cout << "OK" << std::endl;
     return 0;
 }
